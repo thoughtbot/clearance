cf4b9ad12040fa13fa19ca4e38ccee7575c8086b
Josh Nichols <josh@technicalpickles.com>
2008-09-06 18:24:08 -0400

    includedified user controller and test.

--- lib/clearance/users_controller.rb
+++ lib/clearance/users_controller.rb
@@ -1,15 +1,21 @@
 module Clearance
   module UsersController
 
-    before_filter :authenticate
-    before_filter :ensure_user_is_accessing_self, :only => [:edit, :update, :show]
-    
-    filter_parameter_logging :password
+    def self.included(base)
+      base.class_eval do
+        before_filter :authenticate
+        before_filter :ensure_user_is_accessing_self, :only => [:edit, :update, :show]
 
-    private
+        filter_parameter_logging :password
+      private
+        include PrivateInstanceMethods
+      end
+    end
 
-    def ensure_user_is_accessing_self
-      deny_access 'You cannot edit that user.' unless current_user.id.to_i == params[:id].to_i
+    module PrivateInstanceMethods
+      def ensure_user_is_accessing_self
+        deny_access 'You cannot edit that user.' unless current_user.id.to_i == params[:id].to_i
+      end
     end
 
   end
