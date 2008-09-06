31300e9f1af93603c7454ffeb6e10bc30e7c7b82
Josh Nichols <josh@technicalpickles.com>
2008-09-06 19:22:10 -0400

    Working unit_test.

--- lib/clearance/unit_test.rb
+++ lib/clearance/unit_test.rb
@@ -40,137 +40,146 @@ module Clearance
             assert @user.crypted_password != @crypt
           end
         end
-
-        context 'authenticating a user' do
-          context 'with good credentials' do
-            setup do
-              @result = User.authenticate @user.email, 'sekrit'
-            end
-
-            should 'return true' do
-              assert @result
-            end
-          end
-
-          context 'with bad credentials' do
-            setup do
-              @result = User.authenticate @user.email, 'horribly_wrong_password'
-            end
-
-            should 'return true' do
-              assert !@result
-            end
-          end
-        end
-
-        context 'authenticated?' do
-          context 'with good credentials' do
-            setup do
-              @result = @user.authenticated? 'sekrit'
-            end
-
-            should 'return true' do
-              assert @result
-            end
-          end
-
-          context 'with bad credentials' do
-            setup do
-              @result = @user.authenticated? 'horribly_wrong_password'
-            end
-
-            should 'return true' do
-              assert !@result
-            end
-          end
-        end
-
-        context 'encrypt' do
+        
+        context 'A user' do
           setup do
-            password  = 'yadda yadda'
-            @crypted  = @user.encrypt password
-            @expected = Digest::SHA1.hexdigest("--#{password}--")
+            @password = 'sekrit'
+            @salt = 'salt'
+            User.any_instance.stubs(:initialize_salt)
+            @user = Factory(:user, :password => @password, :salt => @salt)
           end
+          
+          context 'authenticating a user' do
+            context 'with good credentials' do
+              setup do
+                @result = User.authenticate @user.email, 'sekrit'
+              end
 
-          should 'create a Hash using SHA1 encryption' do
-            assert_equal @expected, @crypted
-            assert_not_equal 'yadda yadda', @crypted
-          end
-        end
-
-        context 'remember_me!' do
-          setup do
-            assert_nil @user.remember_token
-            assert_nil @user.remember_token_expires_at
-            @user.remember_me!
-          end
-
-          should 'set the remember token and expiration date' do
-            assert_not_nil @user.remember_token
-            assert_not_nil @user.remember_token_expires_at
-          end
-
-          should 'remember_token?' do
-            assert @user.remember_token?
-          end
-
-          context 'forget_me!' do
-            setup do
-              @user.forget_me!
+              should 'return true' do
+                assert @result
+              end
             end
 
-            should 'unset the remember token and expiration date' do
+            context 'with bad credentials' do
+              setup do
+                @result = User.authenticate @user.email, 'horribly_wrong_password'
+              end
+
+              should 'return true' do
+                assert !@result
+              end
+            end
+          end
+          
+          context 'authenticated?' do
+            context 'with good credentials' do
+              setup do
+                @result = @user.authenticated? @password
+              end
+
+              should 'return true' do
+                assert @result
+              end
+            end
+
+            context 'with bad credentials' do
+              setup do
+                @result = @user.authenticated? 'horribly_wrong_password'
+              end
+
+              should 'return true' do
+                assert !@result
+              end
+            end
+          end
+
+          context 'encrypt' do
+            setup do
+              @crypted  = @user.encrypt(@password)
+              @expected = Digest::SHA1.hexdigest("--#{@salt}--#{@password}--")
+            end
+
+            should 'create a Hash using SHA1 encryption' do
+              assert_equal @expected, @crypted
+              assert_not_equal @password, @crypted
+            end
+          end
+
+          context 'remember_me!' do
+            setup do
               assert_nil @user.remember_token
               assert_nil @user.remember_token_expires_at
+              @user.remember_me!
             end
 
-            should 'not remember_token?' do
-              assert ! @user.remember_token?
-            end
-          end
-        end
-
-        context 'remember_token?' do
-          context 'when token expires in the future' do
-            setup do
-              @user.update_attribute :remember_token_expires_at, 2.weeks.from_now.utc
+            should 'set the remember token and expiration date' do
+              assert_not_nil @user.remember_token
+              assert_not_nil @user.remember_token_expires_at
             end
 
-            should 'be true' do
+            should 'remember_token?' do
               assert @user.remember_token?
             end
+
+            context 'forget_me!' do
+              setup do
+                @user.forget_me!
+              end
+
+              should 'unset the remember token and expiration date' do
+                assert_nil @user.remember_token
+                assert_nil @user.remember_token_expires_at
+              end
+
+              should 'not remember_token?' do
+                assert ! @user.remember_token?
+              end
+            end
           end
 
-          context 'when token expired' do
+          context 'remember_token?' do
+            context 'when token expires in the future' do
+              setup do
+                @user.update_attribute :remember_token_expires_at, 2.weeks.from_now.utc
+              end
+
+              should 'be true' do
+                assert @user.remember_token?
+              end
+            end
+
+            context 'when token expired' do
+              setup do
+                @user.update_attribute :remember_token_expires_at, 2.weeks.ago.utc
+              end
+
+              should 'be false' do
+                assert ! @user.remember_token?
+              end
+            end
+          end
+
+          context "User.authenticate with a valid email and password" do
             setup do
-              @user.update_attribute :remember_token_expires_at, 2.weeks.ago.utc
+              @found_user = User.authenticate @user.email, @user.password
             end
 
-            should 'be false' do
-              assert ! @user.remember_token?
+            should "find that user" do
+              assert_equal @user, @found_user
+            end
+          end
+
+          context "When sent authenticate with an invalid email and password" do
+            setup do
+              @found_user = User.authenticate "not", "valid" 
+            end
+
+            should "find nothing" do
+              assert_nil @found_user
             end
           end
         end
 
-        context "User.authenticate with a valid email and password" do
-          setup do
-            @found_user = User.authenticate @user.email, @user.password
-          end
-
-          should "find that user" do
-            assert_equal @user, @found_user
-          end
-        end
-
-        context "When sent authenticate with an invalid email and password" do
-          setup do
-            @found_user = User.authenticate "not", "valid" 
-          end
-
-          should "find nothing" do
-            assert_nil @found_user
-          end
-        end
       end
     end
   end
