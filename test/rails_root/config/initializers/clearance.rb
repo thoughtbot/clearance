612bb57df184ab35d1e8c3e97101d5e4f8fcf151
Dan Croak <dcroak@thoughtbot.com>
2008-09-18 14:48:45 -0400

    updating comment

--- test/rails_root/config/initializers/clearance.rb
+++ test/rails_root/config/initializers/clearance.rb
@@ -1,5 +1,5 @@
-# This simulates loading the shoulda plugin, but without relying on
-# vendor/plugins
+# This simulates loading the clearance gem, but without relying on
+# vendor/gems
 
 clearance_path = File.join(File.dirname(__FILE__), *%w(.. .. .. ..))
 clearance_lib_path = File.join(clearance_path, "lib")
