b5469ac7907a1624af2a5bcba6f02b6a89c993fb
Dan Croak <dcroak@thoughtbot.com>
2009-01-15 19:40:09 -0500

    removing all traces of PROJECT_NAME

--- test/rails_root/config/environment.rb
+++ test/rails_root/config/environment.rb
@@ -1,4 +1,3 @@
-PROJECT_NAME = "clearance"
 require File.join(File.dirname(__FILE__), 'boot')
 require 'md5' # Need this up here to generate the session[:secret] value down there
 
@@ -6,8 +5,8 @@ Rails::Initializer.run do |config|
   config.load_paths += Dir.glob(File.join(RAILS_ROOT, 'vendor', 'gems', '*', 'lib'))
   config.time_zone = 'Eastern Time (US & Canada)'
   config.action_controller.session = {
-    :session_key => "_#{PROJECT_NAME}_session",
-    :secret      => [PROJECT_NAME, 'random', 'words', 'here'].map {|k| Digest::MD5.hexdigest(k) }.join
+    :session_key => "_clearance_session",
+    :secret      => ['clearance', 'random', 'words', 'here'].map {|k| Digest::MD5.hexdigest(k) }.join
   }
   config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
   config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'  
