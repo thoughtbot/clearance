9e499c8cf0df036a3fa037663b2ce09e1e0ec161
eugenebolshakov <eugene.bolshakov@gmail.com>
2009-01-28 11:51:44 -0500

    Added clearance features generator

---
+++ generators/clearance_stories/clearance_stories_generator.rb
@@ -0,0 +1,16 @@
+class ClearanceGenerator < Rails::Generator::Base
+  
+  def manifest
+    record do |m|
+      m.directory File.join("features", "step_definitions")
+      
+      ["features/step_definitions/clearance_steps.rb",
+       "features/signin.feature",
+       "features/signout.feature",       
+       "features/signup.feature"].each do |file|
+        m.file file, file
+       end
+    end
+  end
+  
+end
