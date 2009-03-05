1971d659f7cabf10c1e9f30f0051d75c56501831
Dan Croak <dcroak@thoughtbot.com>
2009-03-05 17:52:43 -0500

    removing users/edit, creating TODO list for conversion to Rails engine

--- generators/clearance/clearance_generator.rb
+++ generators/clearance/clearance_generator.rb
@@ -51,7 +51,6 @@ class ClearanceGenerator < Rails::Generator::Base
 
       m.directory File.join("app", "views", "users")
       ["app/views/users/_form.html.erb",
-       "app/views/users/edit.html.erb",
        "app/views/users/new.html.erb"].each do |file|
         m.file file, file
       end
