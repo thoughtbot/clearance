a91f90f007a09f4a97315bc05a1762c0b65bc46e
Dan Croak <dcroak@thoughtbot.com>
2009-03-05 12:41:46 -0500

    prefer flat to nested contexts in sessions controller test.

--- lib/clearance/test/functional/sessions_controller_test.rb
+++ lib/clearance/test/functional/sessions_controller_test.rb
@@ -17,173 +17,165 @@ module Clearance
               should_display_a_sign_in_form
             end
 
-            context "Given a user" do
-              setup { @user = Factory(:user) }
-
-              context "a POST to #create with good credentials" do
-                setup do
-                  ActionMailer::Base.deliveries.clear
-                  post :create, :session => {
-                                  :email    => @user.email,
-                                  :password => @user.password }
-                end
-
-                should_deny_access(:flash => /User has not confirmed email. Confirmation email will be resent./i)
-                should_respond_with :unauthorized
-
-                should "send the confirmation email" do
-                  assert_not_nil email = ActionMailer::Base.deliveries[0]
-                  assert_match /account confirmation/i, email.subject
-                end
-              end
-            end
-
-            context "Given an email confirmed user" do
+            context "a POST to #create with unconfirmed credentials" do
               setup do
                 @user = Factory(:user)
-                @user.confirm_email!
-              end 
-
-              context "a POST to #create with good credentials" do
-                setup do
-                  post :create, :session => {
-                                  :email    => @user.email,
-                                  :password => @user.password }
-                end
-
-                should_set_the_flash_to /success/i
-                should_redirect_to_url_after_create
-                should_be_signed_in_as { @user }
+                ActionMailer::Base.deliveries.clear
+                post :create, :session => {
+                                :email    => @user.email,
+                                :password => @user.password }
               end
 
-              context "a POST to #create with bad credentials" do
-                setup do
-                  post :create, :session => {
-                                  :email    => @user.email,
-                                  :password => "bad value" }
-                end
+              should_deny_access(:flash => /User has not confirmed email. Confirmation email will be resent./i)
+              should_respond_with :unauthorized
 
-                should_set_the_flash_to /bad/i
-                should_respond_with    :unauthorized
-                should_render_template :new
-                should_not_be_signed_in
-              end
-
-              context "a POST to #create with good credentials and remember me" do
-                setup do
-                  post :create, :session => {
-                                  :email       => @user.email,
-                                  :password    => @user.password,
-                                  :remember_me => '1' }
-                end
-
-                should_set_the_flash_to /success/i
-                should_redirect_to_url_after_create
-                should_be_signed_in_as { @user }
-
-                should 'set the cookie' do
-                  assert ! cookies['remember_token'].empty?
-                end
-
-                should 'set the token in users table' do
-                  assert_not_nil @user.reload.token
-                  assert_not_nil @user.reload.token_expires_at
-                end
-              end
-
-              context "a POST to #create with bad credentials and remember me" do
-                setup do
-                  post :create, :session => {
-                                  :email       => @user.email,
-                                  :password    => "bad value",
-                                  :remember_me => '1' }
-                end
-
-                should_set_the_flash_to /bad/i
-                should_respond_with    :unauthorized
-                should_render_template :new
-                should_not_be_signed_in
-
-                should 'not create the cookie' do
-                  assert_nil cookies['remember_token']
-                end
-
-                should 'not set the remember me token in users table' do
-                  assert_nil @user.reload.token
-                  assert_nil @user.reload.token_expires_at
-                end
-              end
-
-              context "a POST to #create with good credentials and A URL to return back" do
-                context "in the session" do
-                  setup do
-                    @return_url = '/url_in_the_session'
-                    @request.session[:return_to] = @return_url
-                    post :create, :session => {
-                                    :email    => @user.email,
-                                    :password => @user.password }
-                  end
-
-                  should_redirect_to("the return URL") { @return_url }
-                end
-
-                context "in the request" do
-                  setup do
-                    @return_url = '/url_in_the_request'
-                    post :create, :session => {
-                                    :email => @user.email,
-                                    :password => @user.password },
-                                    :return_to => @return_url
-                  end
-
-                  should_redirect_to("the return URL") { @return_url }
-                end
-
-                context "in the request and in the session" do
-                  setup do
-                    @return_url = '/url_in_the_session'
-                    @request.session[:return_to] = @return_url
-                    post :create, :session => {
-                                    :email    => @user.email,
-                                    :password => @user.password },
-                                    :return_to => '/url_in_the_request'
-                  end
-
-                  should_redirect_to("the return URL") { @return_url }
-                end
+              should "send the confirmation email" do
+                assert_not_nil email = ActionMailer::Base.deliveries[0]
+                assert_match /account confirmation/i, email.subject
               end
             end
 
-            public_context do
-              context "logging out again" do
-                setup { delete :destroy }
-                should_redirect_to_url_after_destroy
+            context "a POST to #create with good credentials" do
+              setup do
+                @user = Factory(:email_confirmed_user)
+                post :create, :session => {
+                                :email    => @user.email,
+                                :password => @user.password }
+              end
+
+              should_set_the_flash_to /success/i
+              should_redirect_to_url_after_create
+              should_be_signed_in_as { @user }
+            end
+
+            context "a POST to #create with good credentials and remember me" do
+              setup do
+                @user = Factory(:email_confirmed_user)
+                post :create, :session => {
+                                :email       => @user.email,
+                                :password    => @user.password,
+                                :remember_me => '1' }
+              end
+
+              should_set_the_flash_to /success/i
+              should_redirect_to_url_after_create
+              should_be_signed_in_as { @user }
+
+              should 'set the cookie' do
+                assert ! cookies['remember_token'].empty?
+              end
+
+              should 'set the token in users table' do
+                assert_not_nil @user.reload.token
+                assert_not_nil @user.reload.token_expires_at
               end
             end
 
-            signed_in_user_context do
-              context "a DELETE to #destroy without a cookie" do
-                setup { delete :destroy }
-
-                should_set_the_flash_to(/signed out/i)
-                should_redirect_to_url_after_destroy
+            context "a POST to #create with good credentials and a session return url" do
+              setup do
+                @user = Factory(:email_confirmed_user)
+                @return_url = '/url_in_the_session'
+                @request.session[:return_to] = @return_url
+                post :create, :session => {
+                                :email    => @user.email,
+                                :password => @user.password }
               end
 
-              context 'a DELETE to #destroy with a cookie' do
-                setup do
-                  cookies['remember_token'] = CGI::Cookie.new('token', 'value')
-                  delete :destroy
-                end
+              should_redirect_to("the return URL") { @return_url }
+            end
 
-                should 'delete the cookie' do
-                  assert cookies['remember_token'].nil? || # Rails >= 2.3
-                         cookies['remember_token'].empty?  # Rails <  2.3
-                end
+            context "a POST to #create with good credentials and a request return url" do
+              setup do
+                @user = Factory(:email_confirmed_user)
+                @return_url = '/url_in_the_request'
+                post :create, :session => {
+                                :email => @user.email,
+                                :password => @user.password },
+                                :return_to => @return_url
+              end
 
-                should 'delete the remember me token in users table' do
-                  assert_nil @user.reload.token
-                  assert_nil @user.reload.token_expires_at
-                end
+              should_redirect_to("the return URL") { @return_url }
+            end
+
+            context "a POST to #create with good credentials and a session return url and request return url" do
+              setup do
+                @user = Factory(:email_confirmed_user)
+                @return_url = '/url_in_the_session'
+                @request.session[:return_to] = @return_url
+                post :create, :session => {
+                                :email    => @user.email,
+                                :password => @user.password },
+                                :return_to => '/url_in_the_request'
+              end
+
+              should_redirect_to("the return URL") { @return_url }
+            end
+
+            context "a POST to #create with bad credentials" do
+              setup do
+                post :create, :session => {
+                                :email    => 'bad.email@example.com',
+                                :password => "bad value" }
+              end
+
+              should_set_the_flash_to /bad/i
+              should_respond_with    :unauthorized
+              should_render_template :new
+              should_not_be_signed_in
+            end
+
+            context "a POST to #create with bad credentials and remember me" do
+              setup do
+                post :create, :session => {
+                                :email       => 'bad.email@example.com',
+                                :password    => "bad value",
+                                :remember_me => '1' }
+              end
+
+              should_set_the_flash_to /bad/i
+              should_respond_with    :unauthorized
+              should_render_template :new
+              should_not_be_signed_in
+
+              should 'not create the cookie' do
+                assert_nil cookies['remember_token']
+              end
+            end
+
+            context "signing out given a signed out user" do
+              setup do
+                sign_out
+                delete :destroy
+              end
+              should_redirect_to_url_after_destroy
+            end
+
+            context "a DELETE to #destroy without a cookie" do
+              setup do
+                sign_in_as
+                delete :destroy
+              end
+
+              should_set_the_flash_to(/signed out/i)
+              should_redirect_to_url_after_destroy
+            end
+
+            context 'a DELETE to #destroy with a cookie' do
+              setup do
+                @user = Factory(:email_confirmed_user)
+                cookies['remember_token'] = CGI::Cookie.new('token', 'value')
+                sign_in_as @user
+                delete :destroy
+              end
+
+              should 'delete the cookie' do
+                assert cookies['remember_token'].nil? || # Rails >= 2.3
+                       cookies['remember_token'].empty?  # Rails <  2.3
+              end
+
+              should 'delete the remember me token in users table' do
+                assert_nil @user.reload.token
+                assert_nil @user.reload.token_expires_at
               end
             end
 
