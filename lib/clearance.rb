ce1bffa88d4a5108d3f7887eec5f73e755786057
Nick Quaranto <nick@quaran.to>
2009-04-10 12:30:24 -0400

    Modifying load_routes! so it loads clearance routes after rails routes

--- lib/clearance.rb
+++ lib/clearance.rb
@@ -3,3 +3,13 @@ require 'clearance/extensions/rescue'
 
 require 'clearance/authentication'
 require 'clearance/user'
+
+class ActionController::Routing::RouteSet
+  def load_routes_with_clearance!
+    clearance_routes = File.join(File.dirname(__FILE__), *%w[.. config clearance_routes.rb])
+    add_configuration_file(clearance_routes) unless configuration_files.include? clearance_routes
+    load_routes_without_clearance!
+  end
+
+  alias_method_chain :load_routes!, :clearance
+end
