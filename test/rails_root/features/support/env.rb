8f6ac3dd6fd58bbd192071356f1a7802f5cb4e17
Nick Quaranto <nick@quaran.to>
2009-04-21 21:54:43 -0400

    Upgrading env.rb for latest Cucumber and including factory_girl

--- test/rails_root/features/support/env.rb
+++ test/rails_root/features/support/env.rb
@@ -1,12 +1,13 @@
 # Sets up the Rails environment for Cucumber
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
+require 'factory_girl'
 require 'cucumber/rails/world'
-require 'cucumber/formatters/unicode' # Comment out this line if you don't want Cucumber Unicode support
+require 'cucumber/formatter/unicode' # Comment out this line if you don't want Cucumber Unicode support
 Cucumber::Rails.use_transactional_fixtures
+Cucumber::Rails.bypass_rescue
 
 require 'webrat'
-require "webrat/rails"
 
 Webrat.configure do |config|
   config.mode = :rails
