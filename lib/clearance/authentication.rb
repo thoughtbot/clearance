0255b56001d473e97c6ec9fb3e221160e4dfff43
Dan Croak <dcroak@thoughtbot.com>
2009-08-10 11:13:29 -0400

    documented Clearance::Authentication with YARD

--- lib/clearance/authentication.rb
+++ lib/clearance/authentication.rb
@@ -1,7 +1,7 @@
 module Clearance
   module Authentication
 
-    def self.included(controller)
+    def self.included(controller) # :nodoc:
       controller.send(:include, InstanceMethods)
 
       controller.class_eval do
@@ -11,23 +11,83 @@ module Clearance
     end
 
     module InstanceMethods
+      # User in the current session or remember me cookie
+      #
+      # @return [User, nil]
       def current_user
         @_current_user ||= (user_from_cookie || user_from_session)
       end
 
+      # Is the current user signed in?
+      #
+      # @return [true, false]
       def signed_in?
         ! current_user.nil?
       end
 
+      # Is the current user signed out?
+      #
+      # @return [true, false]
       def signed_out?
         current_user.nil?
       end
 
-      protected
-
+      # Deny the user access if they are signed out.
+      #
+      # @example
+      #   before_filter :authenticate
       def authenticate
         deny_access unless signed_in?
       end
+
+      # Sign user in to session.
+      #
+      # @param [User]
+      #
+      # @example
+      #   sign_in(@user)
+      def sign_in(user)
+        if user
+          session[:user_id] = user.id
+        end
+      end
+
+      # Remember user with cookie.
+      #
+      # @param [User]
+      #
+      # @example
+      #   remember(@user)
+      def remember(user)
+        user.remember_me!
+        cookies[:remember_token] = { :value   => user.token,
+                                     :expires => user.token_expires_at }
+      end
+
+      # Forget user. Should only be used if developer overrides sign out.
+      #
+      # @param [User]
+      #
+      # @example
+      #   forget(@user)
+      def forget(user)
+        user.forget_me! if user
+        cookies.delete(:remember_token)
+        reset_session
+      end
+
+      # Store the current location.
+      # Display a flash message if included.
+      # Redirect to sign in.
+      #
+      # @param [String] optional flash message to display to denied user
+      def deny_access(flash_message = nil)
+        store_location
+        flash[:failure] = flash_message if flash_message
+        redirect_to(new_session_url)
+      end
+
+      protected
 
       def user_from_session
         if session[:user_id]
@@ -48,26 +108,12 @@ module Clearance
         sign_in(user)
       end
 
-      def sign_in(user)
-        if user
-          session[:user_id] = user.id
-        end
-      end
-
       def remember?
         params[:session] && params[:session][:remember_me] == "1"
       end
 
-      def remember(user)
-        user.remember_me!
-        cookies[:remember_token] = { :value   => user.token,
-                                     :expires => user.token_expires_at }
-      end
-
-      def forget(user)
-        user.forget_me! if user
-        cookies.delete(:remember_token)
-        reset_session
+      def store_location
+        session[:return_to] = request.request_uri if request.get?
       end
 
       def redirect_back_or(default)
@@ -85,16 +131,6 @@ module Clearance
 
       def redirect_to_root
         redirect_to(root_url)
-      end
-
-      def store_location
-        session[:return_to] = request.request_uri if request.get?
-      end
-
-      def deny_access(flash_message = nil, opts = {})
-        store_location
-        flash[:failure] = flash_message if flash_message
-        redirect_to(new_session_url)
       end
     end
 
