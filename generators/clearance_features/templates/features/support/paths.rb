34befa87ddd87c960fd89712b432d8e1c90e2605
Dan Croak <dcroak@thoughtbot.com>
2010-01-05 11:32:00 -0500

    updating features/support/paths.rb in features generator to use a couple of niceties from the new cucumber

--- generators/clearance_features/templates/features/support/paths.rb
+++ generators/clearance_features/templates/features/support/paths.rb
@@ -1,22 +1,23 @@
 module NavigationHelpers
   def path_to(page_name)
     case page_name
-    
-    when /the homepage/i
-      root_path
+
+    when /the home\s?page/
+      '/'
     when /the sign up page/i
       new_user_path
     when /the sign in page/i
       new_session_path
     when /the password reset request page/i
       new_password_path
-    
+
     # Add more page name => path mappings here
-    
+
     else
-      raise "Can't find mapping from \"#{page_name}\" to a path."
+      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+        "Now, go and add a mapping in #{__FILE__}"
     end
   end
 end
- 
+
 World(NavigationHelpers)
