8c37063414bb441a777d88be1fc47b39c438fc1a
Dan Croak <dcroak@thoughtbot.com>
2010-04-09 14:14:55 -0400

    cookie_expiration configuration now allows developer to change 'always remember me' opinion to 'never remember me' for a certain class of apps as described by Yardboy. still against the idea of forcing 'remember me' checkbox on users.

--- lib/clearance/authentication.rb
+++ lib/clearance/authentication.rb
@@ -64,7 +64,7 @@ module Clearance
         if user
           cookies[:remember_token] = {
             :value   => user.remember_token,
-            :expires => Clearance.configuration.cookie_duration.from_now.utc
+            :expires => Clearance.configuration.cookie_expiration.call
           }
           self.current_user = user
         end
--- lib/clearance/configuration.rb
+++ lib/clearance/configuration.rb
@@ -1,10 +1,10 @@
 module Clearance
   class Configuration
-    attr_accessor :mailer_sender, :cookie_duration
+    attr_accessor :mailer_sender, :cookie_expiration
 
     def initialize
-      @mailer_sender   = 'donotreply@example.com'
-      @cookie_duration = 1.year
+      @mailer_sender     = 'donotreply@example.com'
+      @cookie_expiration = lambda { 1.year.from_now.utc }
     end
   end
 
@@ -15,10 +15,15 @@ module Clearance
   # Configure Clearance someplace sensible,
   # like config/initializers/clearance.rb
   #
+  # If you want users to only be signed in during the current session
+  # instead of being remembered, do this:
+  #
+  #   config.cookie_expiration = lambda { }
+  #
   # @example
   #   Clearance.configure do |config|
-  #     config.mailer_sender   = 'me@example.com'
-  #     config.cookie_duration = 2.weeks
+  #     config.mailer_sender     = 'me@example.com'
+  #     config.cookie_expiration = lambda { 2.weeks.from_now.utc }
   #   end
   def self.configure
     self.configuration ||= Configuration.new
