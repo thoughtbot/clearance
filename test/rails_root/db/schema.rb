087481f8b5dc9f90860a9b659e4ffbc049582191
Chad Pytel <cpytel@thoughtbot.com>
2010-06-09 16:57:40 -0400

    update gitignore

--- test/rails_root/db/schema.rb
+++ test/rails_root/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100609204229) do
+ActiveRecord::Schema.define(:version => 20100609205423) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
