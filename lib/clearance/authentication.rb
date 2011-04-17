bbe335b09c3dedadb6880663eac1d99aaa6f1322
Dan Croak <dcroak@thoughtbot.com>
2011-04-16 23:39:53 -0400

    use Rails 3's ActiveSupport::Concern to clean up some files

--- lib/clearance/authentication.rb
+++ lib/clearance/authentication.rb
@@ -1,138 +1,128 @@
 module Clearance
   module Authentication
+    extend ActiveSupport::Concern
 
-    def self.included(controller) # :nodoc:
-      controller.send(:include, InstanceMethods)
-      controller.extend(ClassMethods)
+    included do
+      helper_method :current_user, :signed_in?, :signed_out?
+      hide_action   :current_user, :current_user=,
+                    :signed_in?,   :signed_out?,
+                    :sign_in,      :sign_out,
+                    :authenticate, :deny_access
     end
 
-    module ClassMethods
-      def self.extended(controller)
-        controller.helper_method :current_user, :signed_in?, :signed_out?
-        controller.hide_action   :current_user, :current_user=,
-                                 :signed_in?,   :signed_out?,
-                                 :sign_in,      :sign_out,
-                                 :authenticate, :deny_access
-
-      end
+    # User in the current cookie
+    #
+    # @return [User, nil]
+    def current_user
+      @_current_user ||= user_from_cookie
     end
 
-    module InstanceMethods
-      # User in the current cookie
-      #
-      # @return [User, nil]
-      def current_user
-        @_current_user ||= user_from_cookie
-      end
+    # Set the current user
+    #
+    # @param [User]
+    def current_user=(user)
+      @_current_user = user
+    end
 
-      # Set the current user
-      #
-      # @param [User]
-      def current_user=(user)
-        @_current_user = user
-      end
+    # Is the current user signed in?
+    #
+    # @return [true, false]
+    def signed_in?
+      ! current_user.nil?
+    end
 
-      # Is the current user signed in?
-      #
-      # @return [true, false]
-      def signed_in?
-        ! current_user.nil?
-      end
+    # Is the current user signed out?
+    #
+    # @return [true, false]
+    def signed_out?
+      current_user.nil?
+    end
 
-      # Is the current user signed out?
-      #
-      # @return [true, false]
-      def signed_out?
-        current_user.nil?
-      end
+    # Deny the user access if they are signed out.
+    #
+    # @example
+    #   before_filter :authenticate
+    def authenticate
+      deny_access unless signed_in?
+    end
 
-      # Deny the user access if they are signed out.
-      #
-      # @example
-      #   before_filter :authenticate
-      def authenticate
-        deny_access unless signed_in?
-      end
-
-      # Sign user in to cookie.
-      #
-      # @param [User]
-      #
-      # @example
-      #   sign_in(@user)
-      def sign_in(user)
-        if user
-          cookies[:remember_token] = {
-            :value   => user.remember_token,
-            :expires => Clearance.configuration.cookie_expiration.call
-          }
-          self.current_user = user
-        end
-      end
-
-      # Sign user out of cookie.
-      #
-      # @example
-      #   sign_out
-      def sign_out
-        current_user.reset_remember_token! if current_user
-        cookies.delete(:remember_token)
-        self.current_user = nil
-      end
-
-      # Store the current location and redirect to sign in.
-      # Display a failure flash message if included.
-      #
-      # @param [String] optional flash message to display to denied user
-      def deny_access(flash_message = nil)
-        store_location
-        flash[:failure] = flash_message if flash_message
-        redirect_to(sign_in_url)
-      end
-
-      protected
-
-      # CSRF protection in Rails >= 3.0.4
-      # http://weblog.rubyonrails.org/2011/2/8/csrf-protection-bypass-in-ruby-on-rails
-      def handle_unverified_request
-        super
-        sign_out
-      end
-
-      def user_from_cookie
-        if token = cookies[:remember_token]
-          ::User.find_by_remember_token(token)
-        end
-      end
-
-      def sign_user_in(user)
-        warn "[DEPRECATION] sign_user_in: unnecessary. use sign_in(user) instead."
-        sign_in(user)
-      end
-
-      def store_location
-        if request.get?
-          session[:return_to] = request.fullpath
-        end
-      end
-
-      def redirect_back_or(default)
-        redirect_to(return_to || default)
-        clear_return_to
-      end
-
-      def return_to
-        session[:return_to] || params[:return_to]
-      end
-
-      def clear_return_to
-        session[:return_to] = nil
-      end
-
-      def redirect_to_root
-        redirect_to('/')
+    # Sign user in to cookie.
+    #
+    # @param [User]
+    #
+    # @example
+    #   sign_in(@user)
+    def sign_in(user)
+      if user
+        cookies[:remember_token] = {
+          :value   => user.remember_token,
+          :expires => Clearance.configuration.cookie_expiration.call
+        }
+        self.current_user = user
       end
     end
 
+    # Sign user out of cookie.
+    #
+    # @example
+    #   sign_out
+    def sign_out
+      current_user.reset_remember_token! if current_user
+      cookies.delete(:remember_token)
+      self.current_user = nil
+    end
+
+    # Store the current location and redirect to sign in.
+    # Display a failure flash message if included.
+    #
+    # @param [String] optional flash message to display to denied user
+    def deny_access(flash_message = nil)
+      store_location
+      flash[:failure] = flash_message if flash_message
+      redirect_to(sign_in_url)
+    end
+
+    # CSRF protection in Rails >= 3.0.4
+    # http://weblog.rubyonrails.org/2011/2/8/csrf-protection-bypass-in-ruby-on-rails
+    def handle_unverified_request
+      super
+      sign_out
+    end
+
+    protected
+
+    def user_from_cookie
+      if token = cookies[:remember_token]
+        ::User.find_by_remember_token(token)
+      end
+    end
+
+    def sign_user_in(user)
+      warn "[DEPRECATION] sign_user_in: unnecessary. use sign_in(user) instead."
+      sign_in(user)
+    end
+
+    def store_location
+      if request.get?
+        session[:return_to] = request.fullpath
+      end
+    end
+
+    def redirect_back_or(default)
+      redirect_to(return_to || default)
+      clear_return_to
+    end
+
+    def return_to
+      session[:return_to] || params[:return_to]
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
+    end
+
+    def redirect_to_root
+      redirect_to('/')
+    end
   end
 end
--- lib/clearance/user.rb
+++ lib/clearance/user.rb
@@ -2,6 +2,7 @@ require 'digest/sha1'
 
 module Clearance
   module User
+    extend ActiveSupport::Concern
 
     # Hook for all Clearance::User modules.
     #
@@ -9,174 +10,15 @@ module Clearance
     # extend and include à la carte.
     #
     # @example
-    #   extend ClassMethods
-    #   include InstanceMethods
-    #   include AttrAccessor
     #   include Callbacks
     #
-    # @see ClassMethods
-    # @see InstanceMethods
-    # @see AttrAccessor
     # @see Validations
     # @see Callbacks
-    def self.included(model)
-      model.extend(ClassMethods)
+    included do
+      attr_accessor :password, :password_changing
 
-      model.send(:include, InstanceMethods)
-      model.send(:include, AttrAccessor)
-      model.send(:include, Validations)
-      model.send(:include, Callbacks)
-    end
-
-    module AttrAccessor
-      # Hook for attr_accessor virtual attributes.
-      #
-      # :password
-      def self.included(model)
-        model.class_eval do
-          attr_accessor :password
-          private
-          attr_accessor :password_changing
-        end
-      end
-    end
-
-    module Validations
-      # Hook for validations.
-      #
-      # :email must be present, unique, formatted
-      #
-      # If password is required,
-      # :password must be present, confirmed
-      def self.included(model)
-        model.class_eval do
-          validates_presence_of   :email, :unless => :email_optional?
-          validates_uniqueness_of :email, :allow_blank => true
-          validates_format_of     :email, :with => %r{^[a-z0-9!#\$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#\$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$}i, :allow_blank => true
-
-          validates_presence_of   :password, :unless => :password_optional?
-        end
-      end
-    end
-
-    module Callbacks
-      # Hook for callbacks.
-      #
-      # salt, token, password encryption are handled before_save.
-      def self.included(model)
-        model.class_eval do
-          before_validation :downcase_email
-          before_save       :initialize_salt,
-                            :encrypt_password
-          before_create     :generate_remember_token
-        end
-      end
-    end
-
-    module InstanceMethods
-      # Am I authenticated with given password?
-      #
-      # @return [true, false]
-      # @example
-      #   user.authenticated?('password')
-      def authenticated?(password)
-        encrypted_password == encrypt(password)
-      end
-
-      # Set the remember token.
-      #
-      # @deprecated Use {#reset_remember_token!} instead
-      def remember_me!
-        warn "[DEPRECATION] remember_me!: use reset_remember_token! instead"
-        reset_remember_token!
-      end
-
-      # Reset the remember token.
-      #
-      # @example
-      #   user.reset_remember_token!
-      def reset_remember_token!
-        generate_remember_token
-        save(:validate => false)
-      end
-
-      # Mark my account as forgotten password.
-      #
-      # @example
-      #   user.forgot_password!
-      def forgot_password!
-        generate_confirmation_token
-        save(:validate => false)
-      end
-
-      # Update my password.
-      #
-      # @return [true, false] password was updated or not
-      # @example
-      #   user.update_password('new-password')
-      def update_password(new_password)
-        self.password_changing = true
-        self.password          = new_password
-        if valid?
-          self.confirmation_token = nil
-          generate_remember_token
-        end
-        save
-      end
-
-      protected
-
-      def generate_hash(string)
-        Digest::SHA1.hexdigest(string)
-      end
-
-      def encrypt(string)
-        generate_hash("--#{salt}--#{string}--")
-      end
-
-      def initialize_salt
-        if salt.blank?
-          self.salt = generate_hash("--#{Time.now.utc}--#{password}--#{rand}--")
-        end
-      end
-
-      def encrypt_password
-        if password.present?
-          self.encrypted_password = encrypt(password)
-        end
-      end
-
-      def generate_remember_token
-        self.remember_token = encrypt("--#{Time.now.utc}--#{encrypted_password}--#{id}--#{rand}--")
-      end
-
-      def generate_confirmation_token
-        self.confirmation_token = encrypt("--#{Time.now.utc}--#{password}--#{rand}--")
-      end
-
-      # Always false. Override to allow other forms of authentication
-      # (username, facebook, etc).
-      # @return [Boolean] true if the email field be left blank for this user
-      def email_optional?
-        false
-      end
-
-      # True if the password has been set and the password is not being
-      # updated and we are not updating the password. Override to allow
-      # other forms of authentication (username, facebook, etc).
-      # @return [Boolean] true if the password field can be left blank for this user
-      def password_optional?
-        encrypted_password.present? && password.blank? && password_changing.blank?
-      end
-
-      def password_required?
-        # warn "[DEPRECATION] password_required?: use !password_optional? instead"
-        !password_optional?
-      end
-
-      def downcase_email
-        self.email = email.to_s.downcase
-      end
+      include Validations
+      include Callbacks
     end
 
     module ClassMethods
@@ -192,5 +34,144 @@ module Clearance
       end
     end
 
+    module Validations
+      extend ActiveSupport::Concern
+
+      # Hook for validations.
+      #
+      # :email must be present, unique, formatted
+      #
+      # If password is required,
+      # :password must be present, confirmed
+      included do
+        validates_presence_of     :email, :unless => :email_optional?
+        validates_uniqueness_of   :email, :case_sensitive => false, :allow_blank => true
+        validates_format_of       :email, :with => %r{^[a-z0-9!#\$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#\$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$}i, :allow_blank => true
+
+        validates_presence_of     :password, :unless => :password_optional?
+        validates_confirmation_of :password
+      end
+    end
+
+    module Callbacks
+      extend ActiveSupport::Concern
+
+      # Hook for callbacks.
+      #
+      # salt, token, password encryption are handled before_save.
+      included do
+        before_validation :downcase_email
+        before_save   :initialize_salt,
+                      :encrypt_password
+        before_create :generate_remember_token
+      end
+    end
+
+    # Am I authenticated with given password?
+    #
+    # @param [String] plain-text password
+    # @return [true, false]
+    # @example
+    #   user.authenticated?('password')
+    def authenticated?(password)
+      encrypted_password == encrypt(password)
+    end
+
+    # Set the remember token.
+    #
+    # @deprecated Use {#reset_remember_token!} instead
+    def remember_me!
+      warn "[DEPRECATION] remember_me!: use reset_remember_token! instead"
+      reset_remember_token!
+    end
+
+    # Reset the remember token.
+    #
+    # @example
+    #   user.reset_remember_token!
+    def reset_remember_token!
+      generate_remember_token
+      save(:validate => false)
+    end
+
+    # Mark my account as forgotten password.
+    #
+    # @example
+    #   user.forgot_password!
+    def forgot_password!
+      generate_confirmation_token
+      save(:validate => false)
+    end
+
+    # Update my password.
+    #
+    # @param [String, String] password and password confirmation
+    # @return [true, false] password was updated or not
+    # @example
+    #   user.update_password('new-password', 'new-password')
+    def update_password(new_password, new_password_confirmation)
+      self.password_changing     = true
+      self.password              = new_password
+      self.password_confirmation = new_password_confirmation
+      if valid?
+        self.confirmation_token = nil
+        generate_remember_token
+      end
+      save
+    end
+
+    protected
+
+    def generate_hash(string)
+      Digest::SHA1.hexdigest(string)
+    end
+
+    def encrypt(string)
+      generate_hash("--#{salt}--#{string}--")
+    end
+
+    def initialize_salt
+      if salt.blank?
+        self.salt = generate_hash("--#{Time.now.utc}--#{password}--#{rand}--")
+      end
+    end
+
+    def encrypt_password
+      if password.present?
+        self.encrypted_password = encrypt(password)
+      end
+    end
+
+    def generate_remember_token
+      self.remember_token = encrypt("--#{Time.now.utc}--#{encrypted_password}--#{id}--#{rand}--")
+    end
+
+    def generate_confirmation_token
+      self.confirmation_token = encrypt("--#{Time.now.utc}--#{password}--#{rand}--")
+    end
+
+    # Always false. Override to allow other forms of authentication
+    # (username, facebook, etc).
+    # @return [Boolean] true if the email field be left blank for this user
+    def email_optional?
+      false
+    end
+
+    # True if the password has been set and the password is not being
+    # updated and we are not updating the password. Override to allow
+    # other forms of authentication (username, facebook, etc).
+    # @return [Boolean] true if the password field can be left blank for this user
+    def password_optional?
+      encrypted_password.present? && password.blank? && password_changing.blank?
+    end
+
+    def password_required?
+      # warn "[DEPRECATION] password_required?: use !password_optional? instead"
+      !password_optional?
+    end
+
+    def downcase_email
+      self.email = email.to_s.downcase
+    end
   end
 end
