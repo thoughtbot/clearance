c2fcd5e3834efb6b5bb50d1fda6326a38958c6ff
Dan Croak <dcroak@thoughtbot.com>
2011-10-23 22:05:36 -0400

    added another password strategy: a Fake for use in Rails test suites so developers don't pay the time cost of the BCrypt hashing algorithm

--- lib/clearance/password_strategies.rb
+++ lib/clearance/password_strategies.rb
@@ -3,5 +3,6 @@ module Clearance
     autoload :SHA1, 'clearance/password_strategies/sha1'
     autoload :Blowfish, 'clearance/password_strategies/blowfish'
     autoload :BCrypt, 'clearance/password_strategies/bcrypt'
+    autoload :Fake,   'clearance/password_strategies/fake'
   end
 end
