d06565658e8455b2340f53a758798d756c863c8a
Arun Agrawal and Gabe Berke-Williams <support+arun+gabe@thoughtbot.com>
2012-06-13 16:28:09 -0400

    SignedIn constraint yields a signed-in user.

--- lib/clearance/constraints/signed_in.rb
+++ lib/clearance/constraints/signed_in.rb
@@ -1,8 +1,31 @@
 module Clearance
   module Constraints
     class SignedIn
+      def initialize(&block)
+        @block = block
+      end
+
       def matches?(request)
-        request.env[:clearance].signed_in?
+        @request = request
+        signed_in? && current_user_matches_block?
+      end
+
+      private
+
+      def signed_in?
+        @request.env[:clearance].signed_in?
+      end
+
+      def current_user_matches_block?
+        if @block
+          @block.call(current_user)
+        else
+          true
+        end
+      end
+
+      def current_user
+        @request.env[:clearance].current_user
       end
     end
   end
--- spec/clearance/constraints/signed_in_spec.rb
+++ spec/clearance/constraints/signed_in_spec.rb
@@ -21,4 +21,34 @@ describe Clearance::Constraints::SignedIn do
     signed_in_constraint = Clearance::Constraints::SignedIn.new
     signed_in_constraint.matches?(request).should be_false
   end
+
+  it 'yields a signed-in user to a provided block' do
+    user = create(:user, :email => 'before@example.com')
+    cookies = {'action_dispatch.cookies' => {
+      Clearance::Session::REMEMBER_TOKEN_COOKIE => user.remember_token
+    }}
+    env = { :clearance => Clearance::Session.new(cookies) }
+    request = Rack::Request.new(env)
+
+    signed_in_constraint = Clearance::Constraints::SignedIn.new do |user|
+      user.update_attribute(:email, 'after@example.com')
+    end
+
+    signed_in_constraint.matches?(request)
+    user.reload.email.should == 'after@example.com'
+  end
+
+  it 'does not yield a user if they are not signed in' do
+    user = create(:user, :email => 'before@example.com')
+    cookies = {'action_dispatch.cookies' => {}}
+    env = { :clearance => Clearance::Session.new(cookies) }
+    request = Rack::Request.new(env)
+
+    signed_in_constraint = Clearance::Constraints::SignedIn.new do |user|
+      user.update_attribute(:email, 'after@example.com')
+    end
+
+    signed_in_constraint.matches?(request)
+    user.reload.email.should == 'before@example.com'
+  end
 end
