a851db7925f1c572e0b933f732ec413f131f9549
Arun Agrawal and Gabe Berke-Williams <support+arun+gabe@thoughtbot.com>
2012-06-13 15:15:21 -0400

    Add a password strategy to migrate from SHA1 to BCrypt.

--- lib/clearance/password_strategies.rb
+++ lib/clearance/password_strategies.rb
@@ -4,5 +4,6 @@ module Clearance
     autoload :Blowfish, 'clearance/password_strategies/blowfish'
     autoload :BCrypt, 'clearance/password_strategies/bcrypt'
     autoload :Fake,   'clearance/password_strategies/fake'
+    autoload :BCryptMigrationFromSHA1, 'clearance/password_strategies/bcrypt_migration_from_sha1'
   end
 end
