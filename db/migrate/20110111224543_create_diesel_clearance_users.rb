1879b4686a9f37b4cc6e91d6fd2b3eeeb01bf0b3
Mike Burns <mike@mike-burns.com>
2012-07-19 14:11:42 +0200

    Remove the salt from the DB migration

--- db/migrate/20110111224543_create_diesel_clearance_users.rb
+++ db/migrate/20110111224543_create_diesel_clearance_users.rb
@@ -3,7 +3,6 @@ class CreateDieselClearanceUsers < ActiveRecord::Migration
     create_table(:users) do |t|
       t.string   :email
       t.string   :encrypted_password, :limit => 128
-      t.string   :salt,               :limit => 128
       t.string   :confirmation_token, :limit => 128
       t.string   :remember_token,     :limit => 128
       t.timestamps
