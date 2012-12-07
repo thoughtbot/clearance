426a7e91a59cfb5bb33d049dc7e53d0e063d1e3c
Narath Carlile <narathc@gmail.com>
2012-12-07 18:17:05 -0500

    In order for clearance integration tests to work on linux systems (as used by tddium) you need to include all the integration helper files explicity since the sort order on the linux systems is not the same as on mac systems, and without it all specs will fail.

--- lib/generators/clearance/specs/templates/support/integration.rb
+++ lib/generators/clearance/specs/templates/support/integration.rb
@@ -1,3 +1,5 @@
+Dir[Rails.root.join("spec/support/integration/*.rb")].each { |f| require f }
+
 RSpec.configure do |config|
   config.include Integration::ClearanceHelpers, :type => :request
   config.include Integration::ActionMailerHelpers, :type => :request
