a8405b4de7ced81eb073c293a484423c452f8032
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-01-31 19:43:24 -0500

    sign_out should use DELETE, not GET

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,6 @@ Rails.application.routes.draw do
     end
 
   get '/sign_in' => 'clearance/sessions#new', :as => 'sign_in'
-  get '/sign_out' => 'clearance/sessions#destroy', :as => 'sign_out', :via => :delete
+  delete '/sign_out' => 'clearance/sessions#destroy', :as => 'sign_out'
   get '/sign_up' => 'clearance/users#new', :as => 'sign_up'
 end
