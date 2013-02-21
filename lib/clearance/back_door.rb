2999c25892f57e0a1cde7fdc51eee9b65f58cbab
Joe Ferris <jferris@thoughtbot.com>
2013-02-21 13:30:27 -0500

    Add Clearance::BackDoor for signing in quickly in tests
    http://robots.thoughtbot.com/post/37907699673/faster-tests

---
+++ lib/clearance/back_door.rb
@@ -0,0 +1,42 @@
+module Clearance
+  # Middleware which allows signing in by passing as=USER_ID in a query
+  # parameter.
+  #
+  # Designed to eliminate time in integration tests wasted by visiting and
+  # submitting the sign in form.
+  #
+  # Configuration:
+  #
+  #   # config/environments/test.rb
+  #   MyRailsApp::Application.configure do
+  #     # ...
+  #     config.middleware.use ClearanceBackDoor
+  #     # ...
+  #   end
+  #
+  # Usage:
+  #
+  #   visit new_feedback_path(as: user)
+  class BackDoor
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      sign_in_through_the_back_door(env)
+      @app.call(env)
+    end
+
+    private
+
+    def sign_in_through_the_back_door(env)
+      params = Rack::Utils.parse_query(env['QUERY_STRING'])
+      user_id = params['as']
+
+      if user_id.present?
+        user = ::User.find(user_id)
+        env[:session].sign_in(user)
+      end
+    end
+  end
+end
---
+++ spec/clearance/back_door_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe Clearance::BackDoor do
+  it 'signs in as a given user' do
+    user_id = '123'
+    user = stub('user')
+    User.stubs(:find).with(user_id).returns(user)
+    env = env_for_user_id(user_id)
+    back_door = Clearance::BackDoor.new(mock_app)
+
+    result = back_door.call(env)
+
+    env[:session].should have_received(:sign_in).with(user)
+    result.should eq mock_app.call(env)
+  end
+
+  it 'delegates directly without a user' do
+    env = env_without_user_id
+    back_door = Clearance::BackDoor.new(mock_app)
+
+    result = back_door.call(env)
+
+    env[:session].should have_received(:sign_in).never
+    result.should eq mock_app.call(env)
+  end
+
+  def env_without_user_id
+    env_for_user_id('')
+  end
+
+  def env_for_user_id(user_id)
+    session = stub('session', sign_in: true)
+    Rack::MockRequest.env_for("/?as=#{user_id}").merge(session: session)
+  end
+
+  def mock_app
+    lambda { |env| [200, {}, ['okay']] }
+  end
+end
