9ac9b0710f255fc64e59e3ae0d06e1494b621672
Jakub Suder <jakub.suder@gmail.com>
2013-03-25 16:38:00 +0100

    added option to make remember_token cookie secure
    this is important if you have an app running on
    HTTPS, otherwise the auth cookie is leaked when
    you visit a HTTP URL and can be intercepted

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Clearance::Configuration do
+  after { restore_default_config }
+
   describe 'when no user_model_name is specified' do
     before do
       Clearance.configure do |config|
@@ -29,6 +31,29 @@ describe Clearance::Configuration do
 
     it 'is used instead of User' do
       Clearance.configuration.user_model.should == ::MyUser
+    end
+  end
+
+  describe 'when secure_cookie is set to true' do
+    before do
+      Clearance.configure do |config|
+        config.secure_cookie = true
+      end
+    end
+
+    it 'returns true' do
+      Clearance.configuration.secure_cookie.should be_true
+    end
+  end
+
+  describe 'when secure_cookie is not specified' do
+    before do
+      Clearance.configure do |config|
+      end
+    end
+
+    it 'defaults to false' do
+      Clearance.configuration.secure_cookie.should be_false
     end
   end
 end
