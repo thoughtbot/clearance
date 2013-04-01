e3ced5fb4ef67e0010ad3931c06bdf7ef2620715
Greg Lazarev <greg@thoughtbot.com>
2013-04-01 09:47:37 -0700

    Install bundle with `--local` flag in features
    * move gem installation to reader-friedly step
    * take out turn gem, which causes problems with local installs for CI

--- features/step_definitions/gem_file_steps.rb
+++ features/step_definitions/gem_file_steps.rb
@@ -5,3 +5,11 @@ end
 When /^I add the "([^"]*)" gem from this project$/ do |gem_name|
   append_to_file('Gemfile', %{\ngem "#{gem_name}", :path => "../../.."\n})
 end
+
+When /^I remove the "([^"]*)" gem from this project$/ do |gem_name|
+  in_current_dir do
+    content = File.read('Gemfile')
+    content.gsub!(/^.*gem 'turn'.*\n/, '')
+    File.open('Gemfile', 'w') { |file| file.write(content) }
+  end
+end
