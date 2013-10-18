3caae4ca867479027877d11b6e1c13999ff7e5eb
Adarsh Pandit <adarsh@thoughtbot.com>
2013-10-18 15:09:12 -0700

    Add back clearance/testing with deprecation message

---
+++ lib/clearance/testing.rb
@@ -0,0 +1,3 @@
+require 'active_support/deprecation'
+
+ActiveSupport::Deprecation.warn %{`require 'clearance/testing' is deprecated. Please require `clearance/rspec` or `clearance/minitest` instead.}
