02ba479975fb1457fd5f2e5bc94fe2d441c72fd2
Derek Prior <derekprior@gmail.com>
2014-03-20 21:33:57 -0400

    Add a setting to disable all routes
    This setting allows clearance routes to be completely customized. Users who
    customize the routes to this level may have to edit the views and mailers as
    there are references to routes there.

--- spec/routing/clearance_routes_spec.rb
+++ spec/routing/clearance_routes_spec.rb
@@ -1,16 +1,30 @@
 require 'spec_helper'
 
 describe 'routes for Clearance' do
-  context 'signup disabled' do
-    before(:all) do
-      Clearance.configure do |config|
-        config.allow_sign_up = false
-      end
-
+  context 'routes disabled' do
+    around do |example|
+      Clearance.configure { |config| config.routes = false }
+      Rails.application.reload_routes!
+      example.run
+      Clearance.configuration = Clearance::Configuration.new
       Rails.application.reload_routes!
     end
 
-    after :all do
+    it 'does not draw any routes' do
+      expect(get: 'sign_up').not_to be_routable
+      expect(get: 'sign_in').not_to be_routable
+      expect(get: 'passwords/new').not_to be_routable
+      expect(post: 'sessions').not_to be_routable
+      expect(post: 'passwords').not_to be_routable
+      expect(post: 'users').not_to be_routable
+    end
+  end
+
+  context 'signup disabled' do
+    around do |example|
+      Clearance.configure { |config| config.allow_sign_up = false }
+      Rails.application.reload_routes!
+      example.run
       Clearance.configuration = Clearance::Configuration.new
       Rails.application.reload_routes!
     end
