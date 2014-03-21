05c36bcce8be90fa5f7a25f3dc91d3c347e78475
Derek Prior <derekprior@gmail.com>
2014-03-20 22:03:52 -0400

    Update spec style
    Use updated spec style.
    
    * Eliminate before blocks
    * Eliminate let statements
    * Use expect syntax
    * Use method describes rather than contexts

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -3,143 +3,84 @@ require 'spec_helper'
 describe Clearance::Configuration do
   after { restore_default_config }
 
-  context 'when no user_model_name is specified' do
-    before do
-      Clearance.configure do |config|
-      end
-    end
-
+  describe '#user_model' do
     it 'defaults to User' do
       expect(Clearance.configuration.user_model).to eq ::User
     end
-  end
 
-  context 'when a custom user_model_name is specified' do
-    before do
+    it 'uses the specified class when configured' do
       MyUser = Class.new
-
-      Clearance.configure do |config|
-        config.user_model = MyUser
-      end
-    end
-
-    it 'is used instead of User' do
+      Clearance.configure { |config| config.user_model = MyUser }
       expect(Clearance.configuration.user_model).to eq ::MyUser
     end
   end
 
-  context 'when secure_cookie is set to true' do
-    before do
-      Clearance.configure do |config|
-        config.secure_cookie = true
-      end
-    end
-
-    it 'returns true' do
-      expect(Clearance.configuration.secure_cookie).to be_true
-    end
-  end
-
-  context 'when secure_cookie is not specified' do
-    before do
-      Clearance.configure do |config|
-      end
-    end
-
+  describe '#secure_cookie' do
     it 'defaults to false' do
-      expect(Clearance.configuration.secure_cookie).to be_false
+      expect(Clearance.configuration.secure_cookie).to be false
+    end
+
+    it 'is true when configured' do
+      Clearance.configure { |config| config.secure_cookie = true }
+      expect(Clearance.configuration.secure_cookie).to be true
     end
   end
 
-  context 'when no redirect URL specified' do
-    it 'should return "/" as redirect URL' do
+  describe '#redirect_url' do
+    it 'defaults to "/"' do
       expect(Clearance::Configuration.new.redirect_url).to eq '/'
     end
-  end
 
-  context 'when redirect URL is specified' do
-    let(:new_redirect_url) { '/admin' }
-
-    before do
-      Clearance.configure do |config|
-        config.redirect_url = new_redirect_url
-      end
-    end
-
-    it 'should return new redirect URL' do
-      expect(Clearance.configuration.redirect_url).to eq new_redirect_url
+    it 'can be configured' do
+      redirect_url = '/admin'
+      Clearance.configure { |config| config.redirect_url = redirect_url }
+      expect(Clearance.configuration.redirect_url).to eq redirect_url
     end
   end
 
-  context 'when specifying sign in guards' do
-    DummyGuard = Class.new
-
-    before do
-      Clearance.configure do |config|
-        config.sign_in_guards = [DummyGuard]
-      end
-    end
-
-    it 'should return the stack with added guards' do
+  describe '#sign_in_guards' do
+    it 'returns the stack with added guards' do
+      DummyGuard = Class.new
+      Clearance.configure { |config| config.sign_in_guards = [DummyGuard] }
       expect(Clearance.configuration.sign_in_guards).to eq [DummyGuard]
     end
   end
 
-  context 'when cookie domain is specified' do
-    let(:domain) { '.example.com' }
-
-    before do
-      Clearance.configure do |config|
-        config.cookie_domain = domain
-      end
-    end
-
+  describe '#cookie_domain' do
     it 'returns configured value' do
+      domain = 'example.com'
+      Clearance.configure { |config| config.cookie_domain = domain }
       expect(Clearance.configuration.cookie_domain).to eq domain
     end
   end
 
-  context 'when cookie path is specified' do
-    let(:path) { '/user' }
-
-    before do
-      Clearance.configure do |config|
-        config.cookie_path = path
-      end
-    end
-
+  describe '#cookie_path' do
     it 'returns configured value' do
+      path = '/user'
+      Clearance.configure { |config| config.cookie_path = path }
       expect(Clearance.configuration.cookie_path).to eq path
     end
   end
 
   describe '#allow_sign_up?' do
-    context 'when allow_sign_up is configured to false' do
-      it 'returns false' do
-        Clearance.configure { |config| config.allow_sign_up = false }
-        Clearance.configuration.allow_sign_up?.should be_false
-      end
+    it 'defaults to true' do
+      expect(Clearance.configuration.allow_sign_up?).to be true
     end
 
-    context 'when allow_sign_up has not been configured' do
-      it 'returns true' do
-        Clearance.configuration.allow_sign_up?.should be_true
-      end
+    it 'can be configured to false' do
+      Clearance.configure { |config| config.allow_sign_up = false }
+      expect(Clearance.configuration.allow_sign_up?).to be false
     end
   end
 
   describe '#user_actions' do
-    context 'when allow_sign_up is configured to false' do
-      it 'returns empty array' do
-        Clearance.configure { |config| config.allow_sign_up = false }
-        Clearance.configuration.user_actions.should eq []
-      end
+    it 'defaults to [:create]' do
+      expect(Clearance.configuration.user_actions).to eq [:create]
     end
 
-    context 'when sign_up has not been configured' do
-      it 'returns create' do
-        Clearance.configuration.user_actions.should eq [:create]
-      end
+    it 'is empty when sign up is disabled' do
+      Clearance.configure { |config| config.allow_sign_up = false }
+      expect(Clearance.configuration.user_actions).to be_empty
     end
   end
 
@@ -147,19 +88,18 @@ describe Clearance::Configuration do
     it 'returns the parameter key to use based on the user_model' do
       CustomUser = Class.new(ActiveRecord::Base)
       Clearance.configure { |config| config.user_model = CustomUser }
-
-      Clearance.configuration.user_id_parameter.should eq :custom_user_id
+      expect(Clearance.configuration.user_id_parameter).to eq :custom_user_id
     end
   end
 
   describe '#routes_enabled?' do
     it 'is true by default' do
-      Clearance.configuration.routes_enabled?.should be true
+      expect(Clearance.configuration.routes_enabled?).to be true
     end
 
     it 'is false when routes are set to false' do
       Clearance.configure { |config| config.routes = false }
-      Clearance.configuration.routes_enabled?.should be false
+      expect(Clearance.configuration.routes_enabled?).to be false
     end
   end
 end
