30fe8401a2bfc533cc2dcd325c92152a41ad8924
Frank Ashurst <airongstring@gmail.com>
2014-05-04 00:01:49 -0300

    Remove unnecessary autoload call from PasswordStrategies module.

--- lib/clearance/password_strategies.rb
+++ lib/clearance/password_strategies.rb
@@ -4,7 +4,6 @@ module Clearance
     autoload :BCryptMigrationFromSHA1,
       'clearance/password_strategies/bcrypt_migration_from_sha1'
     autoload :Blowfish, 'clearance/password_strategies/blowfish'
-    autoload :Fake, 'clearance/password_strategies/fake'
     autoload :SHA1, 'clearance/password_strategies/sha1'
   end
 end
