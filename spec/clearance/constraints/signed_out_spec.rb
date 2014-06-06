85d39652afcb8c1dcc7ad29d09d2dfc7ef9bf87b
Derek Prior <derekprior@gmail.com>
2014-06-06 09:25:32 -0400

    Update rspec and other gems
    Eliminate rspec deprecation warnings.

--- spec/clearance/constraints/signed_out_spec.rb
+++ spec/clearance/constraints/signed_out_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 describe Clearance::Constraints::SignedOut do
   it 'returns true when user is signed out' do
     signed_out_constraint = Clearance::Constraints::SignedOut.new
-    signed_out_constraint.matches?(request_without_remember_token).should be_true
+    signed_out_constraint.matches?(request_without_remember_token).
+      should be_truthy
   end
 
   it 'returns false when user is not signed out' do
     user = create(:user)
     signed_out_constraint = Clearance::Constraints::SignedOut.new
     signed_out_constraint.matches?(request_with_remember_token(user.remember_token)).
-      should be_false
+      should be_falsey
   end
 end
