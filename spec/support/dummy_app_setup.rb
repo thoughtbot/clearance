48ed7a8cdf1e3853928e87126f22a93227982fff
Derek Prior <derekprior@gmail.com>
2014-06-13 22:12:41 -0400

    Use bin stubs for dummy app setup
    This avoids warnings from bundler about version mismatches and also
    provides quit a speed boost in the test suite over `bundle exec`.

--- spec/support/dummy_app_setup.rb
+++ spec/support/dummy_app_setup.rb
@@ -2,9 +2,9 @@ RSpec.configure do |config|
   config.before :suite, type: :feature do
     Dir.chdir("spec/dummy") do
       `git init . && git add . && git commit -m "commit"`
-      `bundle exec rake db:schema:load`
-      `rails g clearance:install`
-      `RAILS_ENV=test bundle exec rake db:migrate`
+      `bin/rake db:schema:load`
+      `bin/rails g clearance:install`
+      `RAILS_ENV=test bin/rake db:migrate`
     end
 
     Rails.application.reload_routes!
