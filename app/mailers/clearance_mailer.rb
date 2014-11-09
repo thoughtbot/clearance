e8a4676de5a8c99fa050f5b89005583cba645542
Bradley Sheehan <bradpsheehan@gmail.com>
2014-11-09 17:07:13 -0500

    Update mailer hash to ruby 1.9 hash syntax

--- app/mailers/clearance_mailer.rb
+++ app/mailers/clearance_mailer.rb
@@ -1,9 +1,9 @@
 class ClearanceMailer < ActionMailer::Base
   def change_password(user)
     @user = user
-    mail :from => Clearance.configuration.mailer_sender, :to => @user.email,
-         :subject => I18n.t(:subject,
-                            :scope => [:clearance_mailer, :change_password],
-                            :default => 'Change your password')
+    mail from: Clearance.configuration.mailer_sender, to:  @user.email,
+         subject: I18n.t(:subject,
+                          scope:  [:clearance_mailer, :change_password],
+                          default:  'Change your password')
   end
 end
