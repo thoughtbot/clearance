f8ec3216ed501e4ebfa35abdbfa9eff56330ed23
Melissa Xie <melissa@thoughtbot.com>
2015-02-03 16:03:46 -0500

    fix mailer specs

--- spec/mailers/clearance_mailer_spec.rb
+++ spec/mailers/clearance_mailer_spec.rb
@@ -2,37 +2,33 @@ require "spec_helper"
 
 describe ClearanceMailer do
   it "is from DO_NOT_REPLY" do
-    user = create(:user)
-    user.forgot_password!
+    password_reset = create(:password_reset)
 
-    email = ClearanceMailer.change_password(user)
+    email = ClearanceMailer.change_password(password_reset)
 
     expect(Clearance.configuration.mailer_sender).to eq(email.from[0])
   end
 
   it "is sent to user" do
-    user = create(:user)
-    user.forgot_password!
+    password_reset = create(:password_reset)
 
-    email = ClearanceMailer.change_password(user)
+    email = ClearanceMailer.change_password(password_reset)
 
-    expect(email.to.first).to eq(user.email)
+    expect(email.to.first).to eq(password_reset.user_email)
   end
 
   it "sets its subject" do
-    user = create(:user)
-    user.forgot_password!
+    password_reset = create(:password_reset)
 
-    email = ClearanceMailer.change_password(user)
+    email = ClearanceMailer.change_password(password_reset)
 
     expect(email.subject).to include("Change your password")
   end
 
   it "has html and plain text parts" do
-    user = create(:user)
-    user.forgot_password!
+    password_reset = create(:password_reset)
 
-    email = ClearanceMailer.change_password(user)
+    email = ClearanceMailer.change_password(password_reset)
 
     expect(email.body.parts.length).to eq 2
     expect(email.text_part).to be_present
@@ -40,13 +36,13 @@ describe ClearanceMailer do
   end
 
   it "contains a link to edit the password" do
-    user = create(:user)
-    user.forgot_password!
+    password_reset = create(:password_reset)
+    user = password_reset.user
     host = ActionMailer::Base.default_url_options[:host]
     link = "http://#{host}/users/#{user.id}/password/edit" \
       "?token=#{user.confirmation_token}"
 
-    email = ClearanceMailer.change_password(user)
+    email = ClearanceMailer.change_password(password_reset)
 
     expect(email.text_part.body).to include(link)
     expect(email.html_part.body).to include(link)
