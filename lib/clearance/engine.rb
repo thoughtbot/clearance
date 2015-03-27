53f13edf3873e4d0f7933389b50c5d490bd8438f
Derek Prior <derekprior@gmail.com>
2015-03-27 15:39:20 -0400

    Fix formatting

--- lib/clearance/engine.rb
+++ lib/clearance/engine.rb
@@ -1,12 +1,15 @@
-require 'clearance'
-require 'rails'
+require "clearance"
+require "rails"
 
 module Clearance
   class Engine < Rails::Engine
-    initializer 'clearance.filter' do |app|
+    initializer "clearance.filter" do |app|
       app.config.filter_parameters += [:password, :token]
     end
 
-    config.app_middleware.insert_after ActionDispatch::ParamsParser, Clearance::RackSession
+    config.app_middleware.insert_after(
+      ActionDispatch::ParamsParser,
+      Clearance::RackSession
+    )
   end
 end
