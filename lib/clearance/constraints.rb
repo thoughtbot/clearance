3dbe367a57cf0ee3ec3883344240e49abbdf4ad7
Derek Prior <derekprior@gmail.com>
2015-08-29 18:03:10 -0400

    Add an overview of Clearance routing constraints

--- lib/clearance/constraints.rb
+++ lib/clearance/constraints.rb
@@ -1,2 +1,14 @@
 require 'clearance/constraints/signed_in'
 require 'clearance/constraints/signed_out'
+
+module Clearance
+  # Clearance provides Rails routing constraints that can control access and the
+  # visibility of routes at the routing layer. The {Constraints::SignedIn}
+  # constraint can be used to make routes visible only to signed in users. The
+  # {Constraints::SignedOut} constraint can be used to make routes visible only
+  # to signed out users.
+  #
+  # @see http://guides.rubyonrails.org/routing.html#advanced-constraints
+  module Constraints
+  end
+end
