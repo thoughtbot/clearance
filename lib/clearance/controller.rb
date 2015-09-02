526fd2bec410786a6a6f398074bf0f38fa1c19b1
Derek Prior <derekprior@gmail.com>
2015-09-01 20:56:33 -0400

    Add documentation for Clearance::Controller

--- lib/clearance/controller.rb
+++ lib/clearance/controller.rb
@@ -2,6 +2,19 @@ require 'clearance/authentication'
 require 'clearance/authorization'
 
 module Clearance
+  # Adds clearance controller helpers to the controller it is mixed into.
+  #
+  # This exposes clearance controller and helper methods such as `current_user`.
+  # See {Authentication} and {Authorization} documentation for complete
+  # documentation on the methods.
+  #
+  # The `clearance:install` generator automatically adds this mixin to
+  # `ApplicationController`, which is the recommended configuration.
+  #
+  #     class ApplicationController < ApplicationController
+  #       include Clearance::Controller
+  #     end
+  #
   module Controller
     extend ActiveSupport::Concern
 
