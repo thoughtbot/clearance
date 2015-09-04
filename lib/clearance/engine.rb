f370f5c0c387285b917a7a3f74f1236d6a25cf30
Derek Prior <derekprior@gmail.com>
2015-09-04 11:02:15 -0400

    Document the Clearance::Engine class

--- lib/clearance/engine.rb
+++ lib/clearance/engine.rb
@@ -2,6 +2,22 @@ require "clearance"
 require "rails"
 
 module Clearance
+  # Makes Clearance behavior available to Rails apps on initialization. By using
+  # a Rails Engine rather than a Railtie, Clearance can automatically expose its
+  # own routes and views to the hosting application.
+  #
+  # Requiring `clearance` (likely by having it in your `Gemfile`) will
+  # automatically require the engine. You can opt-out of Clearance's internal
+  # routes by using {Configuration#routes=}. You can override the Clearance
+  # views by running `rails generate clearance:views`.
+  #
+  # In addition to providing routes and views, the Clearance engine:
+  #
+  # * Ensures `password` and `token` parameters are filtered out of Rails logs.
+  # * Mounts the {RackSession} middleware in the appropriate location
+  # * Reloads classes referenced in your {Configuration} on every request in
+  #   development mode.
+  #
   class Engine < Rails::Engine
     initializer "clearance.filter" do |app|
       app.config.filter_parameters += [:password, :token]
