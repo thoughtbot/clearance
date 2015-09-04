41d02f6d89d7b2b216607084d4b182abb834d790
Derek Prior <derekprior@gmail.com>
2015-09-04 15:46:56 -0400

    Document Clearance tokens

--- lib/clearance/token.rb
+++ lib/clearance/token.rb
@@ -1,5 +1,12 @@
 module Clearance
+  # Random token used for password reset tokens and password reset tokens.
+  # Clearance tokens are also public API and are inteded to be used anywhere you
+  # need a random token to correspond to a given user (e.g. you added an email
+  # confirmation token).
   class Token
+    # Generate a new random, 20 byte hex token.
+    #
+    # @return [String]
     def self.new
       SecureRandom.hex(20).encode('UTF-8')
     end
