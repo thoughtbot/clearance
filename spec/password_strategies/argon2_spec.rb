282144ad95ae65c8b93be24c43e69221b0016405
Copywright <techniquea2z@gmail.com>
2016-03-10 11:38:14 -0500

    Expect new with proper cost vars

--- spec/password_strategies/argon2_spec.rb
+++ spec/password_strategies/argon2_spec.rb
@@ -14,7 +14,6 @@ describe Clearance::PasswordStrategies::Argon2 do
     end
 
     it "encrypts with Argon2 using default cost in non test environments" do
-      stub_argon2_cost(t_cost: 4, m_cost: 16)
       model_instance = fake_model_with_argon2_strategy
 
       allow(Rails).to receive(:env).
@@ -22,28 +21,19 @@ describe Clearance::PasswordStrategies::Argon2 do
 
       model_instance.password = password
 
-      expect(model_instance.encrypted_password).to eq('proper cost')
+      expect(Argon2::Password).to receive(:new).with(t_cost: 2, m_cost: 16)
     end
 
     it "encrypts with Argon2 using minimum cost in test environment" do
-      stub_argon2_cost(t_cost: 1, m_cost: 4)
       model_instance = fake_model_with_argon2_strategy
 
       model_instance.password = password
 
-      expect(model_instance.encrypted_password).to eq('proper cost')
+      expect(Argon2::Password).to receive(:new).with(t_cost: 1, m_cost: 4)
     end
 
     def stub_argon2_password
       allow_any_instance_of(Argon2::Password).to receive(:create).and_return(encrypted_password)
-    end
-
-    def stub_argon2_cost(t_cost:, m_cost:)
-      allow_any_instance_of(Argon2::Password).to receive(:create).with(
-        password,
-        t_cost: t_cost,
-        m_cost: m_cost
-      ).and_return('proper cost')
     end
 
     def encrypted_password
