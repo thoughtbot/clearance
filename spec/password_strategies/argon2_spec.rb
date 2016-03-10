b790a26a30bbcaaad98d8566fc8dc977befba412
Copywright <techniquea2z@gmail.com>
2016-03-10 11:49:47 -0500

    Fixed spec, looking good

--- spec/password_strategies/argon2_spec.rb
+++ spec/password_strategies/argon2_spec.rb
@@ -19,21 +19,24 @@ describe Clearance::PasswordStrategies::Argon2 do
       allow(Rails).to receive(:env).
         and_return(ActiveSupport::StringInquirer.new("production"))
 
-      model_instance.password = password
+      expect(Argon2::Password).to receive(:new).with(t_cost: 2, m_cost: 16).
+        and_return(Argon2::Password.new(t_cost: 2, m_cost: 16))
 
-      expect(Argon2::Password).to receive(:new).with(t_cost: 2, m_cost: 16)
+      model_instance.password = password
     end
 
     it "encrypts with Argon2 using minimum cost in test environment" do
       model_instance = fake_model_with_argon2_strategy
 
-      model_instance.password = password
+      expect(Argon2::Password).to receive(:new).with(t_cost: 1, m_cost: 4).
+        and_return(Argon2::Password.new(t_cost: 1, m_cost: 4))
 
-      expect(Argon2::Password).to receive(:new).with(t_cost: 1, m_cost: 4)
+      model_instance.password = password
     end
 
     def stub_argon2_password
-      allow_any_instance_of(Argon2::Password).to receive(:create).and_return(encrypted_password)
+      allow_any_instance_of(Argon2::Password).to receive(:create).
+        and_return(encrypted_password)
     end
 
     def encrypted_password
