56443e67e81451e71d07295498cab0d287a7bff6
Ben Woosley <ben.woosley@gmail.com>
2017-10-16 10:06:08 -0400

    Don't call `helper_method` if its not defined
    `helper_method` is not available in API projects, so we should only call
    it when it's available.

--- lib/clearance/authentication.rb
+++ lib/clearance/authentication.rb
@@ -3,7 +3,10 @@ module Clearance
     extend ActiveSupport::Concern
 
     included do
-      helper_method :current_user, :signed_in?, :signed_out?
+      if respond_to?(:helper_method)
+        helper_method :current_user, :signed_in?, :signed_out?
+      end
+
       private(
         :authenticate,
         :current_user,
