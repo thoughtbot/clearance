255151cc2519f5113c30fe39203a4d52104cfefc
IzumiSy <beetle-noise@gmx.com>
2017-11-10 09:51:57 +0000

    Updated to use  to make routings more concise and use  instead of hash-rockets

--- lib/generators/clearance/routes/templates/routes.rb
+++ lib/generators/clearance/routes/templates/routes.rb
@@ -1,12 +1,12 @@
-  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-  resource :session, controller: "clearance/sessions", only: [:create]
+  scope module: :clearance do
+    resources :passwords, only: [:create, :new]
+    resource :session, controller: :sessions, only: [:create]
 
-  resources :users, controller: "clearance/users", only: [:create] do
-    resource :password,
-      controller: "clearance/passwords",
-      only: [:create, :edit, :update]
+    resources :users, only: [:create] do
+      resource :password, controller: :passwords, only: [:create, :edit, :update]
+    end
+
+    get "/sign_in", to: "sessions#new", as: "sign_in"
+    delete "/sign_out", to: "sessions#destroy", as: "sign_out"
+    get "/sign_up", to: "users#new", as: "sign_up"
   end
-
-  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
-  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
-  get "/sign_up" => "clearance/users#new", as: "sign_up"
