f060c7dc472c8797e31cb4962e96c350b1b2fca6
Jon Roberts <jon@emptyflask.net>
2018-10-24 11:17:22 -0500

    cleanup

--- lib/clearance/password_strategies/bcrypt.rb
+++ lib/clearance/password_strategies/bcrypt.rb
@@ -2,18 +2,13 @@ module Clearance
   module PasswordStrategies
     # Uses BCrypt to authenticate users and store encrypted passwords.
     #
-    # The BCrypt cost (the measure of how many key expansion iterations BCrypt
-    # will perform) is automatically set to the minimum allowed value when
-    # Rails is operating in the test environment and the default cost in all
-    # other envionments. This provides a speed boost in tests.
-    #
-  # BCrypt has a `cost` argument which determines how computationally expensive
-  # the hash is to calculate. The higher the cost, the harder it is for
-  # attackers to crack passwords even if they posess a database dump of the
-  # encrypted passwords. Clearance uses the `bcrypt-ruby` default cost except in 
-  # the test environment, where it uses the minimum cost value for speed. If you
-  # wish to increase the cost over the default, you can do so by setting a
-  # higher cost in an initializer: 
+    # BCrypt has a `cost` argument which determines how computationally
+    # expensive the hash is to calculate. The higher the cost, the harder it is
+    # for attackers to crack passwords even if they posess a database dump of
+    # the encrypted passwords. Clearance uses the `bcrypt-ruby` default cost
+    # except in the test environment, where it uses the minimum cost value for
+    # speed. If you wish to increase the cost over the default, you can do so
+    # by setting a higher cost in an initializer:
     # `BCrypt::Engine.cost = 12`
     module BCrypt
       require 'bcrypt'
@@ -30,7 +25,7 @@ module Clearance
         if new_password.present?
           self.encrypted_password = ::BCrypt::Password.create(
             new_password,
-            cost: cost,
+            cost: cost
           )
         end
       end
