7d6def116148d2cdd389c7c05e9119d86e6a1d3a
Matt Jankowski <mjankowski@thoughtbot.com>
2019-02-25 12:55:10 -0500

    Release version 2.0.0.beta1

--- lib/clearance/version.rb
+++ lib/clearance/version.rb
@@ -1,7 +1,3 @@
 module Clearance
-<<<<<<< HEAD
-  VERSION = "1.17.0".freeze
-=======
-  VERSION = "2.0.0.alpha".freeze
->>>>>>> Stop supporting older versions of Ruby and Rails
+  VERSION = "2.0.0.beta1".freeze
 end
