5ef731bb2f4bb35f74b2383e4e759ff0d55a0806
Nathaniel Suchy <me@lunorian.is>
2019-04-09 21:44:14 -0400

    Changed @allowed_envs back to one line

--- lib/clearance/back_door.rb
+++ lib/clearance/back_door.rb
@@ -31,12 +31,7 @@ module Clearance
   #   visit new_feedback_path(as: user)
   class BackDoor
     def initialize(app, &block)
-      @allowed_envs = Clearance.configuration.allowed_envs
-        || [
-              "test",
-              "ci",
-              "development"
-            ]
+      @allowed_envs = Clearance.configuration.allowed_envs || ["test", "ci", "development"]
       unless @allowed_envs.include? ENV["RAILS_ENV"]
         raise "Can't use backdoor outside of explicitly allowed envs"
       end
