3b89f80975753985559f30ece385c485f09e1d36
Matt Jankowski <mjankowski@thoughtbot.com>
2019-11-13 15:46:10 -0500

    Add more explicit test setup around no-auth cookie response

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -4,12 +4,4 @@ class ApplicationController < ActionController::Base
   def show
     render html: "", layout: "application"
   end
-
-  def static_endpoint
-    if params[:use_current_user]
-      current_user # Just trigger the authentication
-    end
-
-    head :ok
-  end
 end
