3106388c16103189df8e1af5745f96ec39e63d3d
Uday Kadaboina <uday88k@gmail.com>
2020-03-03 22:35:52 -0500

    removed code trying to autoload files in deprecated path

--- lib/clearance/password_strategies.rb
+++ lib/clearance/password_strategies.rb
@@ -14,9 +14,5 @@ module Clearance
   # see {Clearance::PasswordStrategies::BCrypt}.
   module PasswordStrategies
     autoload :BCrypt, 'clearance/password_strategies/bcrypt'
-    autoload :BCryptMigrationFromSHA1,
-      'clearance/password_strategies/bcrypt_migration_from_sha1'
-    autoload :Blowfish, 'clearance/password_strategies/blowfish'
-    autoload :SHA1, 'clearance/password_strategies/sha1'
   end
 end
