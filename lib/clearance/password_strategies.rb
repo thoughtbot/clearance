a5079f59fbb529c578d46873dbfc8f4eafab95ea
Uday K <463602+udaykadaboina@users.noreply.github.com>
2020-07-25 16:44:06 -0400

    Remove code trying to autoload files in deprecated path (#886)
    When Trying to use Clearance's [clearance-deprecated_password_stragegies](https://github.com/thoughtbot/clearance-deprecated_password_strategies)
    
    If I change the password_stragegy to older methods, then the app would blowup. Since the Clearance Gem tries to autoload these files from within the gem, when these files were removed in this commit #817, specifically this [commit](https://github.com/thoughtbot/clearance/commit/be755401017f33f251f7c8ad76780e2d128a0f58#diff-ff65403a22f7c166322edeed5df872e2). 
    
    `config.password_strategy = Clearance::PasswordStrategies::BCryptMigrationFromSHA1`

--- lib/clearance/password_strategies.rb
+++ lib/clearance/password_strategies.rb
@@ -15,9 +15,5 @@ module Clearance
   module PasswordStrategies
     autoload :BCrypt, "clearance/password_strategies/bcrypt"
     autoload :Argon2, "clearance/password_strategies/argon2"
-    autoload :BCryptMigrationFromSHA1,
-             "clearance/password_strategies/bcrypt_migration_from_sha1"
-    autoload :Blowfish, "clearance/password_strategies/blowfish"
-    autoload :SHA1, "clearance/password_strategies/sha1"
   end
 end
