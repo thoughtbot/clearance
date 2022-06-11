41d2b40e9c64fe97c6432b381f5fdb40c4f539b8
Dorian Marié <dorian@thoughtbot.com>
2022-06-11 07:24:34 +0200

    Fixes for rails 6.0 and rails 6.1

--- Appraisals
+++ Appraisals
@@ -1,9 +1,12 @@
 appraise "rails_6.0" do
   gem "railties", "~> 6.0"
+  gem "net-smtp", require: false # not bundled in ruby 3.1
+  gem "psych", "< 4" # psych 4 switched from unsafe load to safe load
 end
 
 appraise "rails_6.1" do
   gem "railties", "~> 6.1"
+  gem "net-smtp", require: false # not bundled in ruby 3.1
 end
 
 appraise "rails_7.0" do
--- spec/dummy/application.rb
+++ spec/dummy/application.rb
@@ -4,7 +4,6 @@ module Dummy
   APP_ROOT = File.expand_path("..", __FILE__).freeze
 
   class Application < Rails::Application
-    config.active_record.legacy_connection_handling = false
     config.action_controller.perform_caching = false
     config.action_mailer.default_url_options = { host: "dummy.example.com" }
     config.action_mailer.delivery_method = :test
@@ -18,5 +17,11 @@ module Dummy
     config.paths["config/database"] = "#{APP_ROOT}/config/database.yml"
     config.paths["log"] = "tmp/log/development.log"
     config.paths.add "config/routes.rb", with: "#{APP_ROOT}/config/routes.rb"
+
+    if Rails.version =~ /^6.0/
+      config.active_record.sqlite3.represent_boolean_as_integer = true
+    else
+      config.active_record.legacy_connection_handling = false
+    end
   end
 end
