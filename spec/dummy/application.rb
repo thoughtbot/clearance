90ace30c7e618923c0b3dde26d9f072e0ae0dea3
Dorian Marié <dorian@thoughtbot.com>
2022-06-11 07:29:54 +0200

    hound: =~ to match?

--- spec/dummy/application.rb
+++ spec/dummy/application.rb
@@ -18,7 +18,7 @@ module Dummy
     config.paths["log"] = "tmp/log/development.log"
     config.paths.add "config/routes.rb", with: "#{APP_ROOT}/config/routes.rb"
 
-    if Rails.version =~ /^6.0/
+    if Rails.version.match?(/^6.0/)
       config.active_record.sqlite3.represent_boolean_as_integer = true
     else
       config.active_record.legacy_connection_handling = false
