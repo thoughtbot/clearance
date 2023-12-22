630f1752f4d6e9a186dc511cccd5a5819a5f3077
Gabe Berke-Williams <gabebw@gabebw.com>
2023-12-22 12:33:28 -0800

    Fix typo in Clearance::Token docs (#1000)

--- lib/clearance/token.rb
+++ lib/clearance/token.rb
@@ -1,8 +1,8 @@
 module Clearance
   # Random token used for password reset and remember tokens.
-  # Clearance tokens are also public API and are inteded to be used anywhere you
-  # need a random token to correspond to a given user (e.g. you added an email
-  # confirmation token).
+  # Clearance tokens are also public API and are intended to be used anywhere
+  # you need a random token to correspond to a given user (e.g. you added an
+  # email confirmation token).
   class Token
     # Generate a new random, 20 byte hex token.
     #
