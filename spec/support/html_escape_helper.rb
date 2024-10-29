915ee031e46297e1a1344973a3b774a9eb3d0a28
Sara Jackson <csara@thoughtbot.com>
2024-10-29 09:51:25 -0400

    A fix for the HTML safe translation was added to Rails 7.1

--- spec/support/html_escape_helper.rb
+++ spec/support/html_escape_helper.rb
@@ -1,6 +1,6 @@
 module HTMLEscapeHelper
   def translated_string(key)
-    if [7.0, 7.1].include?(Rails::VERSION::STRING.to_f)
+    if [7.0].include?(Rails::VERSION::STRING.to_f)
       ERB::Util.html_escape_once(I18n.t(key))
     else
       I18n.t(key)
